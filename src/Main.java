c521af34ac45fea5ce9ea2dd5d1e8583a836b67f
mroklom <valentin.marechal05@gmail.com>
2018-07-28 10:01:30 +0200

    Add panel and show it

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import helper.GUIHelper;
+import view.Panel;
+
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Panel panel = new Panel();
+        panel.setBackground(Color.WHITE);
+        panel.setPreferredSize(new Dimension(600,800));
+        GUIHelper.showOnFrame(panel,"test");
+
     }
 }
---
+++ src/view/Panel.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel extends JPanel {
+
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+
+}
